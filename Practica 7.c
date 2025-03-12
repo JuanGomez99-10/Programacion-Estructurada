9ff346117c69a21ad0e866a53952605858bcf2e4
JuanGomez99-10 <juan.gomez7926@alumnos.udg.mx>
2025-03-12 01:36:06 -0600

    Practica 7.c

--- Practica 7.c
+++ Practica 7.c
@@ -25,3 +25,14 @@ int main()
 }
 */
 
+/*
+{
+int matriz[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Elemento [%d][%d]: %d\n", i, j, matriz[i][j]);
+        }
+    }
+    return 0;
+}
+*/
